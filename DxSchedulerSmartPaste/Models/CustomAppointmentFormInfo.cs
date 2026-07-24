97ea00099e49788e583c45c0f38ff84aed575d8d
John Limos <103324036+jvlimos@users.noreply.github.com>
2026-07-24 14:02:36 +0800

    Initial code commit

---
+++ DxSchedulerSmartPaste/Models/CustomAppointmentFormInfo.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.Blazor;
+
+namespace DxSchedulerSmartPaste.Models;
+
+public class CustomAppointmentFormInfo : SchedulerAppointmentFormInfo
+{
+    public CustomAppointmentFormInfo(
+        DxSchedulerAppointmentItem appointmentItem,
+        DxSchedulerDataStorage dataStorage,
+        DxScheduler scheduler)
+        : base(appointmentItem, dataStorage, scheduler) { }
+}
